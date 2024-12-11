b53a9236fd501f104ca2b119c3f6a49509d4af58
mukulpandit86 <mukul.pandit.86@gmail.com>
2024-12-11 21:05:49 +0530

    Signature Verification Project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.preevaluation import load_and_preprocess_images, save_as_npz
 from src.preprocessing import preprocess_images
 from src.training import train_model, save_model
 from src.evaluation import evaluate_model
@@ -25,9 +26,16 @@ def main():
     model_path = os.path.join(model_dir, 'signature_model.h5')
     save_model(model, model_path)
 
+
+    image_data, labels = load_and_preprocess_images(os.path.join(processed_dir,'real'), os.path.join(processed_dir, 'forged'));
+    output_file = os.path.join(processed_dir, 'signature_data.npz')
+
+    # Save the processed data as an .npz file
+    save_as_npz(image_data, labels, output_file)
+
     # Evaluation
     print("Evaluating the model...")
-    evaluate_model(model_path, processed_dir)
+    evaluate_model(model_path, output_file )
 
 if __name__ == "__main__":
     main()
